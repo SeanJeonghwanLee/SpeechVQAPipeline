d1b1282d4ec9ac2bb67cd3568da7c8c92d839991
Sean Lee <ljhwan1997@naver.com>
2023-12-14 20:35:57 +0900

    asr pipe test is done

--- asr/asr_pipe.py
+++ asr/asr_pipe.py
@@ -1,5 +1,6 @@
 from transformers import WhisperProcessor, WhisperForConditionalGeneration
 from datasets import load_dataset, Audio
+import librosa
 import time
 
 
@@ -15,17 +16,43 @@ class ASRecognizer():
 
     def predict(self, sound):
         if self.demo:
-            sampling_rate, data = sound
-        else: 
-            data = sound
-            sampling_rate = 48000
+            origin_sr, data = sound
+        else:
+            data, origin_sr = sound
         
-        input_features = self.processor(data, sampling_rate=sampling_rate, return_tensors="pt").input_features
+        if origin_sr != 16000:
+            data = self._down_sample(data, origin_sr, resample_sr=16000)
+        
+        input_features = self.processor(data, sampling_rate=16000, return_tensors="pt").input_features
         predicted_ids = self.model.generate(input_features, forced_decoder_ids = self.forced_decoder_ids)
         result = self.processor.batch_decode(predicted_ids, skip_special_tokens=True)
         
         return result
 
+    def _down_sample(self, input_wav, origin_sr, resample_sr):
+        resample = librosa.resample(input_wav, orig_sr = origin_sr, target_sr = resample_sr)
+        return resample
+
+
 
 if __name__ == "__main__":
-    pass # pipe test required
+    import yaml
+
+
+    with open('../config.yml', 'r') as f:
+        config = yaml.full_load(f)
+    asr_config = config["asr"]
+    asr_config["demo"] = False
+    print("###config loaded###")
+
+    asr = ASRecognizer(asr_config)
+    print("###Model loaded###")
+
+    # sample audio
+    audio = librosa.load("./sample/New Recording 25.wav", sr=16000)
+
+
+    st = time.time()
+    result = asr.predict(audio)
+    et = time.time()
+    print(f"result ({et-st} sec): {result}")
